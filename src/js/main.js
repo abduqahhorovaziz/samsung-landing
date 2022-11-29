fa8e3c570b65b2509e079a6174e39fd18f92fbd7
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-11-29 17:12:53 +0500

    feat: carousel and tabs section is done with responsive

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,48 @@ closeNavBtn.onclick = function () {
     mobileNav.classList.add('top-[-300%]');
     mobileNav.classList.remove('top-0');
 }
+
+
+const owl = $('.owl-carousel');
+
+owl.owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+});
+
+$('.nextSliderBtn').click(function () {
+    owl.trigger('next.owl.carousel');
+})
+$('.prevSliderBtn').click(function () {
+    owl.trigger('prev.owl.carousel', [300]);
+});
+
+
+let tabsContainer = document.querySelector("#tabs");
+
+let tabTogglers = tabsContainer.querySelectorAll("a");
+console.log(tabTogglers);
+
+tabTogglers.forEach(function(toggler) {
+  toggler.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    let tabName = this.getAttribute("href");
+
+    let tabContents = document.querySelector("#tab-contents");
+
+    for (let i = 0; i < tabContents.children.length; i++) {
+
+      tabTogglers[i].parentElement.classList.remove("border-black", "border-b",  "-mb-px", "opacity-100");  tabContents.children[i].classList.remove("hidden");
+      if ("#" + tabContents.children[i].id === tabName) {
+        continue;
+      }
+      tabContents.children[i].classList.add("hidden");
+
+    }
+    e.target.parentElement.classList.add("border-black", "border-b-4", "-mb-px", "opacity-100");
+  });
+});
+
+document.getElementById("default-tab").click();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    fontFamily:{
+    extend: {
+      backgroundImage: {
+        'slide-one': "url('/src/img/slider/bg-1.jpg')",
+        'slide-two': "url('/src/img/slider/bg-2.jpg')",
+        'slide-three': "url('/src/img/slider/bg-3.jpg')",
+        'slide-four': "url('/src/img/slider/bg-4.jpg')",
+      },
+      boxShadow: {
+        'card-shadow': '0px 20px 150px rgba(193, 193, 193, 0.32)',
+      }
+    },
+    fontFamily: {
       roboto: ['Roboto', 'sans-serif']
     }
   },
